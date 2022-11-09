6affbe27d1883ff4b2b6b2358fa290ff431a5a17
Purushottam.kumar <purushottam.kumar@jktech.com>
2022-11-09 18:04:30 +0530

    Some Modification Done in UI

--- src/app/assesment-screen/assesment-screen.component.ts
+++ src/app/assesment-screen/assesment-screen.component.ts
@@ -1,16 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-assesment-screen',
   templateUrl: './assesment-screen.component.html',
-  styleUrls: ['./assesment-screen.component.css']
+  styleUrls: ['./assesment-screen.component.css'],
 })
 export class AssesmentScreenComponent implements OnInit {
+  arr: any = [];
+  constructor(private _http: HttpClient, private _navigate:Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    
+  gettingDataFromDatabase() {
+    this._http.get('').subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  startAssesment(){
+    this._navigate.navigate(['inter']);
+    console.log("clicked");
+    
+  }
 }
