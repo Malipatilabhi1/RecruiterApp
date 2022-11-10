60696fa4276f0f8a66c9916a7241db656c40dea1
Malipatilabhi1 <abhishek1@jktech.com>
2022-11-10 13:24:38 +0530

    New commit

--- src/app/assesment-screen/assesment-screen.component.ts
+++ src/app/assesment-screen/assesment-screen.component.ts
@@ -17,13 +17,13 @@ export class AssesmentScreenComponent implements OnInit {
   ngOnInit(): void {
     this.getCandidates();
   }
-  
+
   getCandidates(){
     debugger;
     this.http.get<any>('http://localhost:3000/profileManager').subscribe(
       response=>{
         this.arr=response.data;
-        console.log(this.arr); 
+        console.log(this.arr);  
       }
     );
   }
